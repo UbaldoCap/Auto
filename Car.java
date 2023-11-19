218b302ae0fcd144c71ca7c1057632aae3197fdf
Ubaldo <ubaldocapalbo@gmail.com>
2023-11-19 13:39:11 +0100

    end

---
+++ Car.java
@@ -0,0 +1,58 @@
+public class Car {
+    private int cilindrata;
+
+    private String targa;
+
+    private String marca;
+
+    private String modello;
+
+    public Car (int cilindrata, String targa, String marca, String modello) {
+        this.cilindrata = cilindrata;
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    public int getCilindrata() {
+        return cilindrata;
+    }
+
+    public void setCilindrata(int cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "cilindrata=" + cilindrata +
+                ", targa='" + targa + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modello='" + modello + '\'' +
+                '}';
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car(4000, "CP 631BL", "Audi", "RS Q8");
+        System.out.println(car1);
+    }
+}
